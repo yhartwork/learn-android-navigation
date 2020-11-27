f50a9ab7c15e16331b94d3baaac294a166698d52
Yusuf Habibillah Akbar <yusufdesktop@hotmail.com>
2020-11-27 21:12:07 +0700

    Create project navigation

--- app/src/main/java/com/example/learnnavigation/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/learnnavigation/ui/main/SectionsPagerAdapter.java
@@ -29,7 +29,16 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new PlayingFragment();
+                break;
+            case 1:
+                fragment = new FavoriteFragment();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
